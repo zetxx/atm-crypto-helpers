f87ef1eaabe9abad8202af3419cbec69af0ca200
Krasimir Kolev <krasimir.kolev@softwaregroup.com>
2019-12-19 14:17:46 +0200

    chore: tests

--- tests/unit/cases/signData.js
+++ tests/unit/cases/signData.js
@@ -6,23 +6,23 @@ tap.test('signData emv version 4.1', (t) => {
         masterKey: {mkac: '10101010101010101010101010101010', pan: '1010101010101010', panSeqNum: '04', mkEmvVersion: '4.1', type: 'a'},
         sessionKey: {atc: '0001', skEmvVersion: '4.1', treeHeight: 8, branchFactor: 4, parity: 'odd'}
     });
-    t.equal(lib.signData('f01f01f01f01f01f01f01f01', 'hex', {
+    t.equal(lib.signData('f01f01f01f01f01f01f01f01', 'hex', undefined, undefined, {
         paddingMethod: '1',
         masterKey: cardKeys.masterKey,
         sessionKey: cardKeys.sessionKey
     }), 'C21687D53F9F048E', 'some TX data, padding method 1 - 00...00');
-    t.equal(lib.signData('f01f01f01f01f01f01f01f01', 'hex', {
+    t.equal(lib.signData('f01f01f01f01f01f01f01f01', 'hex', undefined, undefined, {
         paddingMethod: '2',
         masterKey: cardKeys.masterKey,
         sessionKey: cardKeys.sessionKey
     }), 'B7BBE6B2CA55AA40', 'some TX data, padding method 2 - 80...00');
 
-    t.equal(lib.signData('0000000010000000000000000710000000000007101302050030901B6A3C00005503A4A082', 'hex', {
+    t.equal(lib.signData('0000000010000000000000000710000000000007101302050030901B6A3C00005503A4A082', 'hex', undefined, undefined, {
         paddingMethod: '1',
         masterKey: cardKeys.masterKey,
         sessionKey: cardKeys.sessionKey
     }), '810682021324857A', 'real TX data, padding method 1 - 00...00');
-    t.equal(lib.signData('0000000010000000000000000710000000000007101302050030901B6A3C00005503A4A082', 'hex', {
+    t.equal(lib.signData('0000000010000000000000000710000000000007101302050030901B6A3C00005503A4A082', 'hex', undefined, undefined, {
         paddingMethod: '2',
         masterKey: cardKeys.masterKey,
         sessionKey: cardKeys.sessionKey
@@ -35,7 +35,7 @@ tap.test('signData emv version vsdc', (t) => {
     var cardKeys = lib.deriveRuntimeCardKeys({
         masterKey: {mkac: '10101010101010101010101010101010', pan: '1010101010101010', panSeqNum: '04', mkEmvVersion: '4.1', type: 'a'}
     });
-    t.equal(lib.signData('0000000010000000000000000710000000000007101302050030901B6A3C00005503A4A082', 'hex', {
+    t.equal(lib.signData('0000000010000000000000000710000000000007101302050030901B6A3C00005503A4A082', 'hex', undefined, undefined, {
         paddingMethod: '1',
         signEmvVersion: 'vsdc',
         masterKey: cardKeys.masterKey
